ed019b1d03008ebd105b25cf415c878aeca4708f
LeoDiNino97 <leonardo.dinino@uniroma1.it>
2026-03-23 15:12:36 +0100

    Sheaf CC logic

--- src/orchestrators/sheaf_cc.py
+++ src/orchestrators/sheaf_cc.py
@@ -7,91 +7,81 @@ from src.orchestrators.base_orchestrator import BaseOrchestrator
 class SheafCC(BaseOrchestrator):
     def __init__(
         self,
-        agents: list[nn.Module],
+        agents: dict[int, nn.Module],
+        neighbors: dict[int, set[int]],
         lr: float,
-        L: torch.Tensor,
-        B: torch.Tensor,
-        n: int,
-        lmb: float = 1.0,
+        weight_decay: float,
     ):
         super().__init__()
-        self.save_hyperparameters()
 
-        # Agents list
-        self.hparams['agents'] = nn.ModuleList(agents)
-
-        # TODO
         # Network description
-        self.hparams['edges'] = None
+        self.hparams['edges'] = list(
+            {
+                tuple(sorted((str(agent), str(neighbor))))
+                for agent in self.hparams['neighbors']
+                for neighbor in self.hparams['neighbors'][agent]
+            }
+        )
 
-    def on_train_epoch_end(self):
-        dataloader = self.trainer.datamodule.train_dataloader()
+        # Optimal transport module dictionary
+        self.local_reference_frames = {
+            agent: torch.eye(self.agents[agent].out_dim)
+            for agent in self.agents
+        }
 
-        self.eval()
-
-        with torch.no_grad():
-            # Reset local aggregators of cross-covariance
-            for agent in self.hparams.agents:
-                agent.reset_epoch_statistics()
-
-            # Compute and aggregate embeddings
-            for batch in dataloader:
-                batch = self._move_batch_to_device(batch)
-
-                output = self(batch)
-
-                for i, j in self.hparams.edges:
-                    self.hparams.agents[i].accumulate_statistics(
-                        output[i][0], output[j][0], self.hparams.agents[j].R
-                    )
-                    self.hparams.agents[j].accumulate_statistics(
-                        output[j][0], output[i][0], self.hparams.agents[i].R
-                    )
-
-            # Perform reference alignment
-            for agent in self.hparams.agents:
-                agent.update_reference_frame()
-
-        self.train()
-
-        return None
-
-    def _shared_eval(
+    @torch.no_grad()
+    def on_train_epoch_end(
         self,
         batch: dict[str, list[torch.Tensor]],
         batch_idx: int,
-        prefix: str,
+        prefix: str,self
     ):
-        """A common step performed in the test and validation step.
-
-        Args:
-            batch : dict[str, list[torch.Tensor]]
-                The current batch.
-            batch_idx : int
-                The batch index.
-            prefix : str
-                The step type for logging purposes.
-
-        Returns:
-            (outputs, total_loss) : tuple[
-                                        dict[int, torch.Tensor],
-                                        torch.Tensor
-                                    ]
-                The tuple with the output of the network and the epoch loss.
+        """Alignment step performed via Kabsch algorithm to cross-covariance terms among neighbors.
         """
-        outputs = self(batch)
 
-        loss = 0
+        # Compute embeddings of the overlapping areas
+        shared_outputs = {
+            (i, j): {
+                i: self.agents[i](
+                    batch[(int(i), int(j))][0],
+                    triplet_mode=False,
+                ),
+                j: self.agents[j](
+                    batch[(int(i), int(j))][1],
+                    triplet_mode=False,
+                ),
+            }
+            for (i, j) in self.hparams['edges']
+        }
 
-        # Compute the personalized loss for each agent
-        for idx, agent in self.hparams.agents.items():
-            loss += agent.compute_loss(outputs[idx])
+        # Temporary buffer
+        local_reference_frames_temp = {
+            agent: torch.zeros((self.agents[agent].out_dim, self.agents[agent].out_dim))
+            for agent in self.hparams['neighbors']
+        }
 
-        # Get the alignment loss by making neirby agents communicate
-        for agent, neighbors in self.hparams.neighbors.items():
-            for neighbor in neighbors:
-                self.communicate(agent, neighbor)
-        pass
+        # Compute cross-covariance factors and their Kabsch polar factor
+        for agent in self.hparams['neighbors']:
+            cross_cov = torch.zeros((self.agents[agent].out_dim, self.agents[agent].out_dim))
+            for neighbors in self.hparams['neighbors'][agent]:
+                # Aggregate cross covariance
+                edge = tuple(sorted((agent, str(neighbor))))
+                cross_cov += (
+                    (shared_outputs[edge][agent].T
+                    @ (self.local_reference_frames[neighbor]
+                        @ shared_outputs[edge][neighbor].T)
+                    ).T
+                )
+
+            # Compute Kabsch polar factor 
+            U, _, Vt = torch.linalg.svd(cross_cov)
+            Sigma_tilde = torch.ones(U.shape[1])
+
+            Sigma_tilde[-1] = torch.linalg.det(U @ Vt)
+            local_reference_frames_temp[agent] = U @ torch.diag(Sigma_tilde) @ Vt
+
+        # Update local reference frames
+        self.local_reference_frames = local_reference_frames_temp
 
     def _shared_eval(
         self,
@@ -113,69 +103,85 @@ class SheafCC(BaseOrchestrator):
             (output, total_loss) : tuple[dict[int, torch.Tensor], torch.Tensor]
                 The tuple with the output of the network and the epoch loss.
         """
-        R = torch.zeros_like(self.hparams.L)
-        main_loss = 0
-        reg_loss = 0
-        output = self(batch)
-        E = torch.cat(
-            [output[agent.idx] for agent in self.hparams.agents], dim=0
-        )
+        private_outputs = self(batch)
 
-        for agent in self.hparams.agents:
-            main_loss += agent.compute_loss(output[agent.idx])
-            R[
-                agent.idx * self.hparams.n : (agent.idx + 1) * self.hparams.n,
-                agent.idx * self.hparams.n : (agent.idx + 1) * self.hparams.n,
-            ] = agent.R
+        # Compute embeddings of the overlapping areas
+        shared_outputs = {
+            (i, j): {
+                i: self.agents[i](
+                    batch[(int(i), int(j))][0],
+                    triplet_mode=False,
+                ),
+                j: self.agents[j](
+                    batch[(int(i), int(j))][1],
+                    triplet_mode=False,
+                ),
+            }
+            for (i, j) in self.hparams['edges']
+        }
 
-        REP = R @ E
+        total_private_loss = 0
+        total_alignment_loss = 0
 
-        # Node mask
-        B = len(self.hparams.agents)
-        T = E.shape[1]
+        # Compute the personalized loss for each agent
+        for idx, agent in self.agents.items():
+            batch_size = batch[int(idx)][0].size(0)
+            private_loss = agent.compute_loss(private_outputs[int(idx)])
 
-        node_vals = E.view(B, self.hparams.n, T)
-        node_mask = node_vals.abs().sum(dim=1) != 0
-
-        # Edge mask
-        # Need it to zero-out edges where at least one
-        # Base-station does not observe the trajectory
-
-        edge_i = torch.tensor(
-            [e[0] for e in self.hparams.edges], device=E.device
-        )
-        edge_j = torch.tensor(
-            [e[1] for e in self.hparams.edges], device=E.device
-        )
-
-        edge_mask = (node_mask[edge_i] & node_mask[edge_j]).float()
-
-        for i in range(self.hparams.n):
-            reg_loss += self.hparams.lmb * torch.sum(
-                (edge_mask * self.hparams.B.T @ REP[i :: self.hparams.n, :])
-                ** 2
+            self.log(
+                f'{prefix}/loss_agent_{idx}',
+                private_loss,
+                on_step=True,
+                on_epoch=True,
+                batch_size=batch_size,
+                prog_bar=False,
             )
 
-        total_loss = main_loss + reg_loss
+            total_private_loss += private_loss
 
         self.log(
-            f'{prefix}/main_loss_epoch',
-            main_loss,
-            on_step=False,
+            f'{prefix}/total_private_loss',
+            total_private_loss,
+            on_step=True,
             on_epoch=True,
+            batch_size=batch_size,
         )
+
+        # Compute the alignment losses
+        for i, j in self.hparams['edges']:
+            aligned_i = shared_outputs[(i,j)][i] @ self.local_reference_frames[i].T
+            aligned_j = shared_outputs[(i,j)][j] @ self.local_reference_frames[j].T
+
+            # Edge specific transport loss
+            alignment_loss = torch.linalg.norm(aligned_i - aligned_j) ** 2
+
+            total_alignment_loss += alignment_loss
+
         self.log(
-            f'{prefix}/reg_loss_epoch', reg_loss, on_step=False, on_epoch=True
+            f'{prefix}/total_alignment_loss',
+            total_alignment_loss,
+            on_step=True,
+            on_epoch=True,
+            batch_size=batch_size,
         )
+
+        total_loss = (
+            total_private_loss + self.hparams['lmb'] * total_transport_loss
+        )
+
         self.log(
-            f'{prefix}/total_loss_epoch',
+            f'{prefix}/total_loss',
             total_loss,
-            on_step=False,
+            on_step=True,
             on_epoch=True,
+            batch_size=batch_size,
+            prog_bar=True,
         )
 
-        return output, total_loss
+        return private_outputs, total_loss
 
+    def communicate(self):
+        pass
 
 if __name__ == '__main__':
     pass
